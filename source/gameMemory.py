c40a7e3f6dc0227faa525f48e88d778f88ad558d
dmechea <daniel.mechea@gmail.com>
2017-12-11 13:21:54 +1000

    QValue Sequence Update working

--- source/gameMemory.py
+++ source/gameMemory.py
@@ -11,3 +11,41 @@ def addToGameMemory(isMaxSize, memoryToAdd, currentGameMemory):
         return (memoryToAdd, *currentGameMemory)[0:-1]
     else:
         return (memoryToAdd, *currentGameMemory)
+
+def preProcessedMemory(state, prediction, actionTook, stepReward):
+    return (state, prediction, actionTook, stepReward)
+
+def addToSequence(currentMemoryStep, currentSequence=None):
+    if currentSequence:
+        return (currentMemoryStep, *currentSequence)
+    else:
+        return (currentMemoryStep, )
+
+
+def learnedValue(reward, nextQ, discount):
+    return reward + (discount * nextQ)
+
+def updateQValue(currentValue, learningRate, learnedVal):
+    return currentValue + learningRate * learnedVal
+
+# turn the variable output to a tuple
+def predictionToList(predictVariable):
+    return list(predictVariable.data)
+
+# Fix this
+def modifyQValues(sequence, learningRate, discount, rePack):
+    currentMem = None
+
+    for index, memory in enumerate(sequence):
+        state, Qvalues, action, reward = memory
+        #most recent memory is terminal so nextQ = 0
+        if index == 0:
+            nextQ = 0
+
+        Qvalues[action] = Qvalues[action] + learningRate*(
+            reward + (discount*nextQ)
+        )
+        nextQ = Qvalues[action]
+
+        currentMem = rePack(memory, currentMem)
+    return currentMem
--- tests/test_gameMemory.py
+++ tests/test_gameMemory.py
@@ -1,5 +1,11 @@
 import unittest
-from sourceimporter import gameMemory
+import sourceimporter
+import numpy as np
+import torch
+
+gameMemory = sourceimporter.gameMemory
+NN = sourceimporter.Networks
+act = sourceimporter.ActionSpace
 
 class TestgameMemoryFuncs(unittest.TestCase):
     def test_addToGameMemory(self):
@@ -15,7 +21,11 @@ class TestgameMemoryFuncs(unittest.TestCase):
             (25, 1, 30, 1),
             )
         maxxedOut = gameMemory.isFull(maxMemorySize, currentGameMemory)
-        result = gameMemory.addToGameMemory(maxxedOut, eventToAdd, currentGameMemory)
+        result = gameMemory.addToGameMemory(
+            maxxedOut,
+            eventToAdd,
+            currentGameMemory,
+        )
         self.assertEqual(result, expected)
 
     def test_addToFullGameMemory(self):
@@ -30,7 +40,11 @@ class TestgameMemoryFuncs(unittest.TestCase):
             (30, 1, 35, 1),
             )
         maxxedOut = gameMemory.isFull(maxMemorySize, currentGameMemory)
-        result = gameMemory.addToGameMemory(maxxedOut, eventToAdd, currentGameMemory)
+        result = gameMemory.addToGameMemory(
+            maxxedOut,
+            eventToAdd,
+            currentGameMemory
+        )
         self.assertEqual(result, expected)
 
     def test_batchSample1(self):
@@ -54,7 +68,95 @@ class TestgameMemoryFuncs(unittest.TestCase):
         self.assertIn(result[0], currentGameMemory)
         self.assertIn(result[1], currentGameMemory)
 
+    def test_addToEmptySequence(self):
+        dummyGameMemory = ( 1, 2, 3, 4 )
+        expected = (
+            (1, 2, 3, 4),
+        )
+        result = gameMemory.addToSequence(dummyGameMemory)
+        self.assertEqual(result, expected)
 
+    def test_addToSingularSequence(self):
+        dummyGameMemory = ( 1, 2, 3, 4 )
+        currentSequence = (
+            (2, 3, 4, 5),
+        )
+        expected = (
+            (1, 2, 3, 4),
+            (2, 3, 4, 5),
+        )
+        result = gameMemory.addToSequence(dummyGameMemory, currentSequence)
+        self.assertEqual(result, expected)
+
+    def test_addToMultipleSequence(self):
+        dummyGameMemory = ( 1, 2, 3, 4 )
+        currentSequence = (
+            (2, 3, 4, 5),
+            (3, 4, 5, 6),
+        )
+        expected = (
+            (1, 2, 3, 4),
+            (2, 3, 4, 5),
+            (3, 4, 5, 6),
+        )
+        result = gameMemory.addToSequence(dummyGameMemory, currentSequence)
+        self.assertEqual(result, expected)
+
+    def test_QValueSequenceUpdate(self):
+        learningRate = 0.5
+        discount = 0.9
+        state1 = np.array([1, 2, 3, 4])
+        state2 = np.array([2, 3, 4, 5])
+        state3 = np.array([3, 4, 5, 6])
+        InitQValues1 = [0, 0]
+        InitQValues2 = [0, 0]
+        InitQValues3 = [0, 0]
+        ExpectedQ1 = [0, 0]
+        ExpectedQ2 = [0, 0.5]
+        ExpectedQ3 = [0.725, 0]
+        Action1 = 0
+        Action2 = 1
+        Action3 = 0
+        Reward1 = 0
+        Reward2 = 1
+        Reward3 = 1
+
+        mem3 = gameMemory.preProcessedMemory(
+            state3, InitQValues3, Action3, Reward3
+        )
+        mem2 = gameMemory.preProcessedMemory(
+            state2, InitQValues2, Action2, Reward2
+        )
+        mem1 = gameMemory.preProcessedMemory(
+            state1, InitQValues1, Action1, Reward1
+        )
+        currentSeqMem = gameMemory.addToSequence(mem3)
+        currentSeqMem = gameMemory.addToSequence(mem2, currentSeqMem)
+        currentSeqMem = gameMemory.addToSequence(mem1, currentSeqMem)
+
+        result = gameMemory.modifyQValues(
+            currentSeqMem,
+            learningRate,
+            discount,
+            gameMemory.addToSequence
+        )
+
+        # Expected Result
+        mem3 = gameMemory.preProcessedMemory(
+            state3, ExpectedQ3, Action3, Reward3
+        )
+        mem2 = gameMemory.preProcessedMemory(
+            state2, ExpectedQ2, Action2, Reward2
+        )
+        mem1 = gameMemory.preProcessedMemory(
+            state1, ExpectedQ1, Action1, Reward1
+        )
+
+        ExpectedSeqMem = gameMemory.addToSequence(mem1)
+        ExpectedSeqMem = gameMemory.addToSequence(mem2, ExpectedSeqMem)
+        ExpectedSeqMem = gameMemory.addToSequence(mem3, ExpectedSeqMem)
+
+        self.assertEqual(result, ExpectedSeqMem)
 
 if __name__ == '__main__':
     unittest.main()
